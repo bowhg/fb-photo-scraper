dae8f10ba2a4fec7d923b539b02d54c8fc9ba774
Thitiphan Sonsubanan <pepepopapapepepo@gmail.com>
2018-05-14 02:18:45 +0700

    hotfix

--- photo-scraper.rb
+++ photo-scraper.rb
@@ -1,14 +1,15 @@
 require 'koala'
 require 'open-uri'
 
-access_token = "your-access-token"
+access_token = "EAACEdEose0cBADZCidClkVhsckp8k0bPtlp6W3EsLf4HND7ApTfWZCjDCfXPHXBVgmiLl7MtGLLnMZCHd3nx5k98QZCR3C1ufhclJ9HXivFU99ZC2xYWds4ZCdliHTwL9P17aWwcF9uUdg819sZCMlSH0JY7SBQZBZATJmXFkgmcX58x0ZBOgZAm5oZCCvfkgLeuIssZD"
+
 
 @fb = Koala::Facebook::API.new(access_token)
 
-albums = @fb.get_connections("me","albums")
+albums = @fb.get_connections("medium","albums")
 albums.each do |album|
   album_name = album["name"]
-  images = fb.get_object("#{album["id"]}?fields=photos{picture,images}")
+  images = @fb.get_object("#{album["id"]}?fields=photos{picture,images}")
   count = images["photos"]["data"].count
   c = 1
   images["photos"]["data"].each do |image|
